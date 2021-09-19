d8c6824f75a27e78486b2f8ddd1d808c18376732
lunaboona <hyaluna@gmail.com>
2021-09-19 19:19:59 -0300

    add recipe getById

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { Recipe } from '../tokens';
 
 @Injectable({ providedIn: 'root' })
@@ -29,6 +30,25 @@ export class RecipeService {
         description:
           'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores eos vitae tempore explicabo laboriosam voluptatem ut blanditiis, perspiciatis animi mollitia hic provident nemo dignissimos iste id eaque fugit corporis culpa!',
       },
-    ]);
+    ]).pipe(delay(1000));
+  }
+
+  public getById(id: string): Observable<Recipe> {
+    // TODO implement http call
+    // temporarily return mock data
+    return of({
+      name: 'Milkshake de Morango',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores eos vitae tempore explicabo laboriosam voluptatem ut blanditiis, perspiciatis animi mollitia hic provident nemo dignissimos iste id eaque fugit corporis culpa!',
+      tags: [
+        {
+          name: 'Verão',
+          color: '#ffb400',
+        },
+        {
+          name: 'test',
+        },
+      ],
+    }).pipe(delay(1000));
   }
 }
