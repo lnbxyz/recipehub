be0e289950a0581be676758327f6f61421139b94
lunaboona <hyaluna@gmail.com>
2021-09-22 19:49:46 -0300

    add temporary login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'rh-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private userService: UserService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  mockLogin() {
+    this.userService.login({
+      username: 'lunabunna',
+      password: '123qwe',
+    });
   }
 
+  mockLogout() {
+    this.userService.logout();
+  }
 }
