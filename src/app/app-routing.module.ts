76c9489f7746266e1bd066789dd8c4a3a11757da
lunaboona <hyaluna@gmail.com>
2021-09-22 19:50:32 -0300

    add auth route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
     path: '',
     loadChildren: () =>
       import('./pages/pages.module').then((m) => m.PagesModule),
