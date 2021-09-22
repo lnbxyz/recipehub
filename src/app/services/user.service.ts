8cd4494cad236c94f1fff6b298d5c2afdb1771c0
lunaboona <hyaluna@gmail.com>
2021-09-22 19:28:26 -0300

    add user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../tokens';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  public isLoggedIn = new Subject<boolean>();
+
+  public get currentUser(): User | undefined {
+    const data = localStorage.getItem('login');
+    return data ? JSON.parse(data) : undefined;
+  }
+
+  constructor(public http: HttpClient) {}
+
+  public create(user: User): Observable<any> {
+    return this.http.post(`${environment.apiPath}/user`, user);
+  }
+
+  public update(user: User): Observable<any> {
+    return this.http.put(`${environment.apiPath}/user/${user.id}`, user);
+  }
+
+  public delete(id: string): Observable<any> {
+    return this.http.delete(`${environment.apiPath}/user/${id}`);
+  }
+
+  public login({
+    username,
+    password,
+  }: {
+    username: string;
+    password: string;
+  }): void {
+    this.logout();
+    this.http
+      .post(`${environment.apiPath}/user/login`, {
+        username,
+        password,
+      })
+      .subscribe((result) => {
+        if (result) {
+          localStorage.setItem('login', JSON.stringify(result));
+          this.isLoggedIn.next(true);
+        }
+      });
+  }
+
+  public logout(): void {
+    localStorage.removeItem('login');
+    this.isLoggedIn.next(false);
+  }
+}
