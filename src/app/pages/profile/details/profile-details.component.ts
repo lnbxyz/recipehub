73d5c22a5685943713b0211b1ad7bce0742b0581
lunaboona <hyaluna@gmail.com>
2021-09-22 23:37:34 -0300

    implement profile page

--- src/app/pages/profile/details/profile-details.component.ts
+++ src/app/pages/profile/details/profile-details.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'rh-profile-details',
   templateUrl: './profile-details.component.html',
-  styleUrls: ['./profile-details.component.scss']
+  styleUrls: ['./profile-details.component.scss'],
 })
 export class ProfileDetailsComponent implements OnInit {
+  constructor(public userService: UserService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public onEditInfoButtonPressed(): void {
+    // TODO
   }
-
 }
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileDetailsComponent } from './details/profile-details.component';
 import { ProfileRoutingModule } from './profile-routing.module';
+import { ButtonModule } from 'src/app/components/button/button.module';
 
 @NgModule({
   declarations: [ProfileDetailsComponent],
-  imports: [CommonModule, ProfileRoutingModule],
+  imports: [CommonModule, ProfileRoutingModule, ButtonModule],
 })
 export class ProfileModule {}
