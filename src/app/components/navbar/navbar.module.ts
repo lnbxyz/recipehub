f69050ea97578d944bed2b14f8a38be856d3c414
lunaboona <hyaluna@gmail.com>
2021-09-22 23:30:15 -0300

    add sign out icon

--- src/app/components/navbar/navbar.module.ts
+++ src/app/components/navbar/navbar.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { RouterModule } from '@angular/router';
-import { ButtonModule } from '../button/button.module';
 
 @NgModule({
   declarations: [NavbarComponent],
