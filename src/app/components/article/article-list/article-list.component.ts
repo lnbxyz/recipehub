7275314f384909a32cc19c8d3557c8be8af36f5e
lunaboona <hyaluna@gmail.com>
2021-09-26 17:58:58 -0300

    create article list

---
+++ src/app/components/article/article-list/article-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rh-article-list',
+  templateUrl: './article-list.component.html',
+  styleUrls: ['./article-list.component.scss']
+})
+export class ArticleListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/article/article.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArticleListComponent } from './article-list/article-list.component';
+
+@NgModule({
+  declarations: [ArticleListComponent],
+  imports: [CommonModule],
+  exports: [ArticleListComponent],
+})
+export class ArticleModule {}
