0935e057c479c461dea737a905bcd41a2de60675
lunaboona <hyaluna@gmail.com>
2021-09-26 19:03:13 -0300

    implement recipe card

--- src/app/tokens/interfaces/article.interface.ts
+++ src/app/tokens/interfaces/article.interface.ts
@@ -2,6 +2,7 @@ import { Entity } from './entity.interface';
 import { AuditedEntity } from './audited-entity.interface';
 import { MustHaveOwner } from './must-have-owner.interface';
 import { Recipe } from './recipe.interface';
+import { User } from './user.interface';
 
 export interface Article extends AuditedEntity, Entity, MustHaveOwner {
   name: string;
@@ -10,4 +11,5 @@ export interface Article extends AuditedEntity, Entity, MustHaveOwner {
   likeCount?: number;
   liked?: boolean;
   commentCount?: number;
+  user: User;
 }
