0649a4bb2a07ecc8b46e39832fd6da3d06fac9a8
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2021-09-27 22:14:08 -0300

    loading and empty-message in article-list

--- src/app/components/article/article.module.ts
+++ src/app/components/article/article.module.ts
@@ -5,10 +5,12 @@ import { ArticleCardComponent } from './card/article-card.component';
 import { DialogModule } from '../dialog/dialog.module';
 import { InteractionButtonModule } from '../interaction-button/interaction-button.module';
 import { RouterModule } from '@angular/router';
+import { LoadingModule } from 'src/app/components/loading/loading.module';
+import { EmptyMessageModule } from 'src/app/components/empty-message/empty-message.module';
 
 @NgModule({
   declarations: [ArticleListComponent, ArticleCardComponent],
-  imports: [CommonModule, InteractionButtonModule, DialogModule, RouterModule],
+  imports: [CommonModule, InteractionButtonModule, DialogModule, RouterModule, LoadingModule, EmptyMessageModule],
   exports: [ArticleListComponent, ArticleCardComponent],
 })
 export class ArticleModule {}
