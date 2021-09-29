394534c7be2d1d7cc082eb8009bc5256b4eca289
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2021-09-28 22:37:02 -0300

    profile page

--- src/app/pages/profile/details/profile-details.component.ts
+++ src/app/pages/profile/details/profile-details.component.ts
@@ -8,13 +8,17 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./profile-details.component.scss'],
 })
 export class ProfileDetailsComponent implements OnInit {
+  public userId?: string;
+
   constructor(
     public userService: UserService,
     private router: Router,
     private route: ActivatedRoute
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userId = this.userService.currentUser?.id;
+  }
 
   public onEditInfoButtonPressed(): void {
     this.router.navigate(['edit'], { relativeTo: this.route });
